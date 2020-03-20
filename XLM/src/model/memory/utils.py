5aec679baac444325a38c424a52bccae342cb0a8
Patrick Lewis <plewis@fb.com>
2020-03-20 15:27:19 +0000

    missing headers

--- XLM/src/model/memory/utils.py
+++ XLM/src/model/memory/utils.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2020-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
 import sys
 import math
 import numpy as np
--- pytorch-transformers/pseudoalignment/pseudo_decomp_utils.py
+++ pytorch-transformers/pseudoalignment/pseudo_decomp_utils.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2020-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
 from spacy.language import Language
 from tqdm import tqdm
 import os
